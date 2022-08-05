a8499eeb5f1e0c3c95ae20045d19d379dd2e2490
Edgar René Ruiz López <45479530+CrookedTimber@users.noreply.github.com>
2022-08-05 11:05:29 +0100

    add provisional model for Trip item

--- artemis_camping/api/models.py
+++ artemis_camping/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+# Pending
+class Message(models.Model):
+    pass
+
+
+    """Uses coordinates for working with Google Places API and Open Weather API"""
+    """Dates data needs to be adjusted to work with Open Weather API """
+class Trip(models.Model):
+    name = models.CharField("Trip Name", max_length=50)
+    start_latitude = models.DecimalField(
+        max_digits=11, decimal_places=7, null=False, blank=True
+    )
+    start_longitude = models.DecimalField(
+        max_digits=11, decimal_places=7, null=False, blank=True
+    )
+    destination_latitude = models.DecimalField(
+        max_digits=11, decimal_places=7, null=False, blank=True
+    )
+    destination_longitude = models.DecimalField(
+        max_digits=11, decimal_places=7, null=False, blank=True
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
+    difficulty = models.IntegerChoices("Difficulty", "Easy Medium Hard")
