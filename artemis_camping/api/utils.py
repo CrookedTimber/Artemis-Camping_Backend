e1669165bf1f96376e6c39ca3af9df9f838a390e
Edgar René Ruiz López <45479530+CrookedTimber@users.noreply.github.com>
2022-08-08 11:56:19 +0100

    Enabled commented out code

--- artemis_camping/api/utils.py
+++ artemis_camping/api/utils.py
@@ -81,7 +81,7 @@ def get_trip_places(lat, lon):
         for place in place_type["results"]:  
             for type in place["types"]:
                 if type not in listed_types:
-                    Place.objects.create(name=place["name"], type=categories_string_list[counter], latitude=place["geometry"]["location"]["lat"], longitude=place["geometry"]["location"]["lng"])
+                    Place.objects.create(name=place["name"], type=categories_string_list[counter], latitude=place["geometry"]["location"]["lat"], longitude=place["geometry"]["location"]["lng"], place_id=place["id"])
         listed_types.append(categories_string_list[counter])
         counter += 1
     
