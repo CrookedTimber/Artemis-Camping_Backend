cf0a951affe9ca6976daa3b664a4b2aafa51116b
wingyuen2022 <wingyuenuk330@gmail.com>
2022-08-10 00:47:45 +0100

    Add https://artemis-camping.netlify.app in CORS_ALLOWED_ORIGINS

--- artemis_camping/settings.py
+++ artemis_camping/settings.py
@@ -171,5 +171,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://artemis-camping.netlify.app"
 ]
