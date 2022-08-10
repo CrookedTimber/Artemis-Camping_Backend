fa1ce41869f1de32fb9c6b557539532541fb5c25
Summira Hussain <summira508411@gmail.com>
2022-08-10 16:34:49 +0100

    token updated

--- artemis_camping/settings.py
+++ artemis_camping/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 AUTH_USER_MODEL = 'users.UserAccount'
